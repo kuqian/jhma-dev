e4789303ff8ef4e4c21ec4a83348bdd5038f787a
kun qian <stkun.qian@gmail.com>
2018-10-17 14:16:09 -0700

    credential

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
+const app = express();
 app.get('/', (req, res) => {
     res.send({hi: "there"});
 });

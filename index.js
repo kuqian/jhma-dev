1fd6e454acd3245db69f9c247a5ebe0dced603e4
kun qian <stkun.qian@gmail.com>
2018-10-17 15:01:17 -0700

    verification 2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 const app = express();
 // passport.use(new GoogleStrategy( {}, function(){}))
-// app.get('/', (req, res) => {
-//     res.send({hi: "what"});
-// });
 app.get('/', (req, res) => {
+    res.send({hi: "what"});
+});
+app.get('/google60e985a04c73366b.html', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/google60e985a04c73366b.html'));
 });
 const PORT = process.env.PORT || 5000;
